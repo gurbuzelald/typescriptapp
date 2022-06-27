b42d9a24a0bfe06c86074c3d9498a8355ba44b28
Gürbüz <73827791+gurbuzelald@users.noreply.github.com>
2022-06-27 16:12:29 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+],
   theme: {
-    extend: {
-      screens: {
-        "3xl": "1920px",
-      },
-      height: {
-        "1/8": "12.5%",
-        "3/8": "37.5%",
-      },
-      gridTemplateRows: {
-        8: "repeat(8, minmax(0, 1fr))",
-        10: "repeat(10, minmax(0, 1fr))",
-      },
-      gridRow: {
-        "span-7": "span 7 / span 7",
-        "span-9": "span 9 / span 9",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
-};
+  plugins: [],
+}
